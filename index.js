f2d58e1897393354d88e6172005b46631ee140ae
negabaro <negabaro@naver.com>
2020-12-08 14:03:20 +0900

    env,secret test

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
-  // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
+  const res = `Hello ${nameToGreet}!`;
+  const resEnv = `env is ${process.env.HELLO_ENV}`;
+  const resSecret = `secret is ${process.env.HELLO_SECRET}`;
+  //console.log(res);
+  core.setOutput("res", res);
+  core.setOutput("resEnv", resEnv);
+  core.setOutput("resSecret", resSecret);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  //const payload = JSON.stringify(github.context.payload, undefined, 2);
+  //console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
